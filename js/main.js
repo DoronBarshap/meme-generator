33a945d4dc02202f5f0420de3de99b874312dfec
Doron Barshap <doron.prog@gmail.com>
2021-12-15 18:02:16 +0200

    third commit

--- js/main.js
+++ js/main.js
@@ -5,20 +5,101 @@ var gCurrAction = 'text'
 
 
 function onInit() {
-    gCanvas = document.querySelector('#canvas');
-    gCtx = gCanvas.getContext('2d');
-    
+    // gCanvas = document.querySelector('#canvas');
+    // gCtx = gCanvas.getContext('2d');
+
     console.log('i am connected to js');
 
+    renderGallery()
 }
-// renderGallery()
 // renderEditor(gMeme)
 
-function onImageClicked(ev, imageId) {
-    console.log('ev: ', ev)
-    console.log('imageId: ', imageId)
-    renderEditor(imageId)
 
+
+
+function onChangeText(ev) {
+    let elText = document.querySelector('input').value
+    drawText(elText, 40, 40)
+}
+
+
+function renderEditor(imageId) {
+    let strHTML = `
+    <section class="edit-screen">
+                <nav>
+                <button>text size</button>
+                <button>text color</button>
+                <button>blah blah</button>
+                </nav>
+                <canvas id="canvas" height="350" width="350" style="border: 1px solid black">>
+                </canvas>
+                <section class="control-panel">
+                <input placeholder="Add cool text here" onchange="onChangeText(event)" type="text">
+                <button onClick="onFontBigger">text +</button>
+                <button onClick="onFontSmaller">text -</button>
+                <button onClick="onClearCanvas()">Clear</button>
+
+                <button onClick="onChangeTextColor">text color</button>
+                <button onClick="onChangeStrokeColor">stroke color</button>
+                <button onClick="onChangeTextLineUp">to upper line</button>
+                <button onClick="onChangeTextLineDown">to bottom line</button>
+                <button onClick="onAddNewTextLine">add another line</button>
+                <button style="color:green"> <a href="#" onclick="onDownloadImg(this)" download="my-img.jpg">Download</a></button>
+                <button  onClick="onInit()">RESTART</button>
+                <select class="font">
+                <option value="impact">IMPACT</option>
+                <option value="font2">font 2</option>
+                <option value="font3">font 3</option>
+                <option value="font4">font 4</option>
+                </select>
+            </section>
+
+            </section>
+    `
+    document.querySelector('.main-container').innerHTML = strHTML
+    gCanvas = document.querySelector('#canvas');
+    gCtx = gCanvas.getContext('2d');
+    drawImgFromLocal(imageId)
+}
+
+
+
+
+function drawText(txt, x, y) {
+    // gCtx.font = '48px serif';
+    // gCtx.fillText(txt, x, y);
+    gCtx.textBaseline = 'start';
+    gCtx.textAlign = 'start';
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = 'white';
+    gCtx.font = '50px monospace';
+    gCtx.fillStyle = 'red';
+    gCtx.fillText(txt, x, y);
+    gCtx.strokeText(txt, x, y);
+}
+
+function createMeme(imageId) {
+    var meme =
+    {
+        selectedImgId: imageId,
+        selectedLineIdx: 0,
+        lines: [
+            {
+                txt: 'I sometimes eat Falafel',
+                txtSize: 20,
+                font: 'IMPACT',
+                align: 'left',
+                color: 'white',
+                stroke: 'black'
+            }
+        ]
+    }
+}
+console.log('I sometimes eat Falafel'.length);
+// chooses the image and shows it in gallery - need to create a new meme
+function onImageClicked(ev, imageId) {
+    renderEditor(imageId)
+    createMeme(imageId)
 }
 //uploads images from gImages and add them with a forEach method
 function renderGallery() {
@@ -33,90 +114,23 @@ function renderGallery() {
     strHTML += `</section></div>`
     document.querySelector('.main-container').innerHTML = strHTML
 }
-
-function onChangeText(ev) {
-    let elText = document.querySelector('input').value
-    
-    drawText(elText, 50, 50)
-}
-
-
-function renderEditor(imageId) {
-    
-    let strHTML = `
-    <section class="edit-screen">
-                <nav>
-                <button>text size</button>
-                <button>text color</button>
-                <button>blah blah</button>
-                </nav>
-                <canvas id="canvas" height="350" width="350" style="border: 1px solid black">>
-                </canvas>
-                <section class="control-panel">
-                <input placeholder="Add cool text here" onchange="onChangeText(event)" type="text">
-                <button onClick="onFontBigger">text +</button>
-                <button onClick="onFontSmaller">text -</button>
-
-                <button onClick="onChangeTextColor">text color</button>
-                <button onClick="onChangeStrokeColor">stroke color</button>
-                <button onClick="onChangeTextLineUp">to upper line</button>
-                <button onClick="onChangeTextLineDown">to bottom line</button>
-                <button onClick="onAddNewTextLine">add another line</button>
-                <button onClick="onClearCanvas">Clear</button>
-                <select class="font">
-                <option value="impact">IMPACT</option>
-                <option value="font2">font 2</option>
-                <option value="font3">font 3</option>
-                <option value="font4">font 4</option>
-                </select>
-
-            </section>
-
-            </section>
-    `
-    document.querySelector('.main-container').innerHTML = strHTML
-    drawImgFromLocal(imageId)
-}
-
-//draws an image on the canvas // still does'nt work when i send an image..weird!!!
+//draws an image on the canvas 
 function drawImgFromLocal(imageId) {
     var img = new Image()
     // var source = `img/meme-imgs-square/${imageId}.jpg`
-    var source = `img/meme-imgs-square/1.jpg`
-    console.log('source: ',source)  
+    var source = `img/meme-imgs-square/${imageId + 1}.jpg`
+    console.log('source: ', source)
     img.src = source
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
     }
 }
-
-
-
-
-function draw(ev) {
-    // const offsetX = ev.offsetX;
-    // const offsetY = ev.offsetY;
-    const { offsetX, offsetY } = ev
-    switch (gCurrAction) {
-        case 'emogi':
-            //add emogi
-            break;
-        case 'text':
-            //add text line
-            drawText('שלום', offsetX, offsetY);
-            break;
-    }
+// downloads the meme
+function onDownloadImg(elLink) {
+    var imgContent = gCanvas.toDataURL('image/jpeg')
+    elLink.href = imgContent
 }
-
-function drawText(txt, x, y) {
-    // gCtx.font = '48px serif';
-    // gCtx.fillText(txt, x, y);
-    gCtx.textBaseline = 'start';
-    gCtx.textAlign = 'center';
-    gCtx.lineWidth = 2;
-    gCtx.strokeStyle = 'white';
-    gCtx.font = '50px monospace';
-    gCtx.fillStyle = 'red';
-    gCtx.fillText(txt, x, y);
-    gCtx.strokeText(txt, x, y);
+//clears the canvas
+function onClearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
 }
